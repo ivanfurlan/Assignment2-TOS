6511f87bc265b8ee546cfd47f908afc72b9d48f6
Ivan Furlan <ivanfurlan98@gmail.com>
2019-12-08 19:03:18 +0100

    Close #5

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -5,16 +5,27 @@ package it.unipd.tos.business;
 
 import java.util.List;
 import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.ItemType;
 import it.unipd.tos.model.MenuItem;
 
 public class TakeAwayBillImpl implements TakeAwayBill {
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
-        double totale=0;
+        double totale = 0;
+        int countPanini = 0;
+        double prezzoPaninoMenoCaro = 0;
         
         for(MenuItem element : itemsOrdered) {
             totale += element.getPrezzo();
+            if(element.getTipo()==ItemType.PANINO) {
+                if(countPanini == 0 || element.getPrezzo() < prezzoPaninoMenoCaro)
+                    prezzoPaninoMenoCaro = element.getPrezzo();
+                countPanini++;
+            }
         }
-
+        
+        if(countPanini>=5)
+            totale-=prezzoPaninoMenoCaro/2;
+        
         return totale;
     }
 }
